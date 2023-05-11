37fe2a0dba6b554111e3388503680b886c159515
Zoneeox <rajarevaneri@gmail.com>
2023-05-11 22:30:15 +0700

    test

--- src/components/Craft.jsx
+++ src/components/Craft.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Modal from "./Modal.jsx";
 
 const Craft = () => {
   const [isModal, setIsModal] = useState(false);
@@ -33,30 +34,33 @@ const Craft = () => {
     const dataValue = e.target.getAttribute("data-value");
     const currentObject = inputList.find((x) => x.id == dataValue);
 
-    console.log(currentObject);
+    setIsModal((prevState) => !prevState);
   }
 
   return (
-    <div className="bg-[#1B1B1B] w-[40vw] min-h-[20vh] p-10 font-Bebas">
-      <div>
-        <h1 className="text-2xl tracking-wider cursor-default select-none text-white/50">
-          Boom~ Calculator
-        </h1>
-        <div className="flex flex-row gap-3 mt-10">
-          {inputList.map((item, index) => (
-            <div
-              className="w-24 h-24 bg-[#2F3228]/50 hover:bg-[#2F3228] cursor-pointer"
-              data-value={item.id}
-              onClick={(e) => openModal(e)}
-            />
-          ))}
+    <>
+      {isModal ? <Modal setIsModal={setIsModal} /> : ""}
+      <div className="bg-[#1B1B1B] w-[40vw] min-h-[20vh] p-10 font-Bebas">
+        <div>
+          <h1 className="text-2xl tracking-wider cursor-default select-none text-white/50">
+            Raid Calculator
+          </h1>
+          <div className="flex flex-row gap-3 mt-10">
+            {inputList.map((item, index) => (
+              <div
+                className="w-24 h-24 bg-[#2F3228]/50 hover:bg-[#2F3228] cursor-pointer"
+                data-value={item.id}
+                onClick={(e) => openModal(e)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex items-center mt-20 text-2xl text-white/50">
+          <h1>Required Materials</h1>
         </div>
       </div>
-
-      <div className="flex items-center mt-20 text-2xl text-white/50">
-        <h1>Required</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Modal = () => {
-  return <div className="bg-[#121918] min-w-[20vw] min-h-[40vh]">Modal</div>;
+import c4 from "../assets/picture/c4.png";
+import explosive from "../assets/picture/explosive.png";
+import rocket from "../assets/picture/rocket.png";
+import satchel from "../assets/picture/satchel.png";
+
+const Modal = ({ setIsModal }) => {
+  const [items, setItems] = useState([
+    {
+      name: "c4",
+      picture: c4,
+    },
+    {
+      name: "explosive",
+      picture: explosive,
+    },
+    {
+      name: "rocket",
+      picture: rocket,
+    },
+    {
+      name: "satchel",
+      picture: satchel,
+    },
+  ]);
+  const [image, setImage] = useState("");
+  const [active, setActive] = useState(false);
+
+  function closeModal() {
+    setIsModal((prevState) => !prevState);
+  }
+
+  function imageSet(e) {
+    setImage(e.target.src);
+    setActive((prevState) => !prevState);
+  }
+
+  return (
+    <div className="fixed flex items-center justify-center w-screen h-screen p-10 bg-black/20 backdrop-blur-md font-Bebas">
+      <div className="bg-neutral-800 min-w-[30vw] h-fit p-10">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl tracking-wider opacity-50">
+            Available Booms
+          </h1>
+          <p
+            className="text-2xl tracking-wider cursor-pointer text-red-400/50"
+            onClick={closeModal}
+          >
+            Close
+          </p>
+        </div>
+        <div className="grid grid-cols-4 gap-10 pt-10">
+          {items.map((items, id) => (
+            <div className="p-2 rounded cursor-pointer bg-neutral-900 w-fit h-fit hover:bg-neutral-700">
+              <img
+                src={items.picture}
+                alt={items.name}
+                className="w-14"
+                onClick={(e) => imageSet(e)}
+              />
+            </div>
+          ))}
+          <div className="flex flex-row gap-2">
+            <img
+              src={image}
+              alt=""
+              className="w-8 p-1 cursor-pointer bg-white/10 hover:bg-white/20 border-r-[1px] border-white/50"
+            />
+            <input
+              type="text"
+              className="col-span-4 p-1 font-sans bg-neutral-700 active:outline-none focus:outline-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
